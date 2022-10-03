befda6334b5ca4064768dfcfb16b78c5896d04ce
eunseolYun <skopdt73@gmail.com>
2022-10-03 15:44:36 +0900

    WebSocket

--- src/server.js
+++ src/server.js
@@ -1,21 +1,29 @@
-import express, { application } from "express";
-import livereloadMiddleware from "connect-livereload";
-import livereload from "livereload";
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
 
 const app = express();
-
-const liveServer = livereload.createServer({
-  exts: ["js", "pug", "css"],
-  delay: 1000,
-});
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-
-liveServer.watch(__dirname);
-app.use(livereloadMiddleware());
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+// create Websocket server on top of the http server. both server share same port(3000).
+
+wss.on("connection", (socket) => {
+  console.log("✅ Connected to Browser");
+  socket.on("close", () => console.log("❌ Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.send("hello Browser!");
+});
+// socket in server.js(backend) mean connected 'browser'.
+
+server.listen(3000, handleListen);
